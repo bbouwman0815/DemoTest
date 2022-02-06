80b61e1b3651b41b78d3f04e10e9c50306fed2b5
bbouwman0815 <bbouwma1@my.westga.edu>
2022-02-05 20:44:20 -0500

    added everything

---
+++ CapstoneWebsiteDemo/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using SharedLibraryDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CapstoneWebsiteDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private CapstoneDemoDatabaseEntities db = new CapstoneDemoDatabaseEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Login()
+        {
+            ViewBag.Message = "Your login page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login([Bind(Include = "StudentID,LastName,FirstName,EnrollmentDate")] Student student)
+        {
+            if (ModelState.IsValid)
+            {
+                if (student.StudentID == null)
+                {
+                    return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+                }
+                Student student2 = db.Students.Where(s => s.LastName == student.LastName).Where(s => s.FirstName == student.FirstName).FirstOrDefault<Student>();
+                if (student2 == null)
+                {
+                    return HttpNotFound();
+                }
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+    }
+}
---
+++ WPFdemo/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using SharedLibraryDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPFdemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private CapstoneDemoDatabaseEntities db = new CapstoneDemoDatabaseEntities();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            FillBS();
+        }
+
+        private void FillBS()
+        {
+            Student student2 = db.Students.Where(s => s.LastName == "Bouwman").Where(s => s.FirstName == "Brian").FirstOrDefault<Student>();
+            this.textBlock.Text = student2.FirstName;
+        }
+    }
+}
